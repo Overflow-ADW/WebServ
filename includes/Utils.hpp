72f36ae107fa0d519af0a094f09257fe6638ccdb
Lucas Birlouet <lbirloue@shi-r8-p5.s19.be>
2025-06-11 11:07:38 +0200

    supp fonct useless

--- includes/Utils.hpp
+++ includes/Utils.hpp
@@ -11,21 +11,10 @@ public:
     static std::string toUpperCase(const std::string& str);
     
     static bool fileExists(const std::string& path);
-    static bool isDirectory(const std::string& path);
     static std::string readFile(const std::string& path);
-    static bool writeFile(const std::string& path, const std::string& content);
     static std::string getFileExtension(const std::string& path);
     static std::string getMimeType(const std::string& extension);
-    
-    static std::string urlDecode(const std::string& url);
-    static std::string urlEncode(const std::string& url);
-    static std::string getContentType(const std::string& file_path);
-    
-    static std::string getCurrentDateTime();
-    static std::string formatHttpDate(time_t timestamp);
-    
-    static std::string getClientIP(int socket_fd);
-    
+                
 private:
     Utils();
     static std::map<std::string, std::string> _mime_types;
--- src/utils/Utils.cpp
+++ src/utils/Utils.cpp
@@ -57,13 +57,6 @@ bool Utils::fileExists(const std::string& path) {
     return file.good();
 }
 
-bool Utils::isDirectory(const std::string& path) {
-    struct stat statbuf;
-    if (stat(path.c_str(), &statbuf) != 0)
-        return false;
-    return S_ISDIR(statbuf.st_mode);
-}
-
 std::string Utils::readFile(const std::string& path) {
     std::ifstream file(path.c_str(), std::ios::binary);
     if (!file.is_open())
@@ -77,15 +70,6 @@ std::string Utils::readFile(const std::string& path) {
     file.read(&content[0], size);
     
     return content;
-}
-
-bool Utils::writeFile(const std::string& path, const std::string& content) {
-    std::ofstream file(path.c_str(), std::ios::binary);
-    if (!file.is_open())
-        return false;
-    
-    file.write(content.c_str(), content.length());
-    return file.good();
 }
 
 std::string Utils::getFileExtension(const std::string& path) {
@@ -104,74 +88,6 @@ std::string Utils::getMimeType(const std::string& extension) {
     if (it != _mime_types.end())
         return it->second;
     return "application/octet-stream";
-}
-
-std::string Utils::getContentType(const std::string& file_path) {
-    std::string extension = getFileExtension(file_path);
-    return getMimeType(extension);
-}
-
-std::string Utils::getCurrentDateTime() {
-    time_t now = time(0);
-    char buffer[100];
-    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", localtime(&now));
-    return std::string(buffer);
-}
-
-std::string Utils::formatHttpDate(time_t timestamp) {
-    char buffer[100];
-    strftime(buffer, sizeof(buffer), "%a, %d %b %Y %H:%M:%S GMT", gmtime(&timestamp));
-    return std::string(buffer);
-}
-
-std::string Utils::urlDecode(const std::string& url) {
-    std::string result;
-    for (size_t i = 0; i < url.length(); ++i) {
-        if (url[i] == '%' && i + 2 < url.length()) {
-            char hex[3];
-            hex[0] = url[i + 1];
-            hex[1] = url[i + 2];
-            hex[2] = '\0';
-            
-            char* endptr;
-            long val = strtol(hex, &endptr, 16);
-            if (endptr == hex + 2) {
-                result += static_cast<char>(val);
-                i += 2;
-            }
-            else
-                result += url[i];
-        }
-        else if (url[i] == '+') 
-            result += ' ';
-        else
-            result += url[i];
-    }
-    return result;
-}
-
-std::string Utils::urlEncode(const std::string& url) {
-    std::string result;
-    for (size_t i = 0; i < url.length(); ++i) {
-        char c = url[i];
-        if (std::isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~')
-            result += c;
-        else {
-            char hex[4];
-            sprintf(hex, "%%%02X", static_cast<unsigned char>(c));
-            result += hex;
-        }
-    }
-    return result;
-}
-
-std::string Utils::getClientIP(int socket_fd) {
-    struct sockaddr_in addr;
-    socklen_t addr_len = sizeof(addr);
-    
-    if (getpeername(socket_fd, (struct sockaddr*)&addr, &addr_len) == 0)
-        return std::string(inet_ntoa(addr.sin_addr));
-    return "127.0.0.1";
 }
 
 void Utils::initMimeTypes() {
